b5ff72a04827f29b8b0b98cbd90bfc9d117daa94
tumobi <tumobi@163.com>
2018-06-10 15:20:08 +0800

    feat 添加 json_decode 函数

---
+++ test/json/json_decode.test.js
@@ -0,0 +1,9 @@
+require('should')
+const json_decode = require('../../lib/json/json_decode')
+
+describe('json_decode.js', function () {
+  it('json_decode', function () {
+    const obj = json_decode('{"name":"your name","age":11}')
+    obj.name.should.equal('your name')
+  })
+})
